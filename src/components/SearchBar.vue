d2ea988a2004ebf46d98d6fb827fdf76b843c724
suhail23599 <suhail23599@gmail.com>
2023-08-15 18:13:24 +0530

    fix

--- src/components/SearchBar.vue
+++ src/components/SearchBar.vue
@@ -86,6 +86,8 @@ export default {
       this.showModal = true
       if (this.keyword) {
         this.debounceSearch(e.target.value)
+      } else {
+        this.searchResult = []
       }
     },
     handleKeyEvent (event) {
@@ -106,7 +108,7 @@ export default {
         }
       } else if (keyCode === 13) {
         this.showModal = false
-        if(this.activeIndex !== null) {
+        if (this.activeIndex !== null) {
           this.keyword = this.searchResult[this.activeIndex].name
         }
       } else {
